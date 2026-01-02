83df3af7f1d25fc738b3482c540c20331a86a7d7
Emilio Font <emifont01@gmail.com>
2026-01-02 13:40:44 -0400

    chore: use better icons

--- config/config.go
+++ config/config.go
@@ -15,8 +15,8 @@ func Parse() *Config {
 
 	flag.StringVar(&cfg.Client, "client", "ivpn", "VPN client type (e.g., ivpn)")
 	flag.StringVar(&cfg.Action, "action", "status", "Action to perform: status or disconnect")
-	flag.StringVar(&cfg.IconConnected, "icon-connected", "\uf0e5", "Icon for connected state (Nerd Font)")
-	flag.StringVar(&cfg.IconDisconnected, "icon-disconnected", "\uf0e6", "Icon for disconnected state (Nerd Font)")
+	flag.StringVar(&cfg.IconConnected, "icon-connected", "\uf510", "Icon for connected state (Nerd Font)")
+	flag.StringVar(&cfg.IconDisconnected, "icon-disconnected", "\uf512", "Icon for disconnected state (Nerd Font)")
 	flag.StringVar(&cfg.TooltipFormat, "tooltip-format", "Connected to {name} ({ip})", "Tooltip format: use {name} and {ip} placeholders")
 
 	flag.Parse()
